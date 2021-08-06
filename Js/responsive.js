945be5bee89ceb80c36184a84f0d7914c15cac6d
Hadiullah Akib <hadiullahakib46@gmail.com>
2021-08-06 10:41:55 +0600

    nav transition and separate teachers

--- Js/responsive.js
+++ Js/responsive.js
@@ -1,6 +1,8 @@
 var burger = document.querySelector(".burger");
 var mainNav = document.querySelector(".main-nav");
+var mainHeader = document.querySelector(".main-header");
 
 burger.addEventListener("click", () => {
+  mainHeader.classList.toggle("h-class-resp");
   mainNav.classList.toggle("v-class-resp");
 });
